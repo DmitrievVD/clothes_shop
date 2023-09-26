764cb51299af43c1a5302737c315a0883012443a
Viktor <fokus11548@mail.ru>
2023-09-26 20:58:25 +0400

    Complate menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuActive = document.querySelector('menu-bar');
+const borger = document.querySelector('head-menu');
+
+console.dir(document.querySelector('body'))
+
+function toggleMenu(){
+    menuActive.classList.toggle("hidden");
+}
+
+borger.addEventListener('click', toggleMenu);
